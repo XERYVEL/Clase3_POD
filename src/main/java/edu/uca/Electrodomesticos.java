b2ea0ff017e7e89a14acc7b4374f24291bad94bb
Lucìa <francodobrito.samsung@gmail.com>
2025-08-18 21:20:51 -0300

    Primera rama

---
+++ src/main/java/edu/uca/Electrodomesticos.java
@@ -0,0 +1,13 @@
+package edu.uca;
+
+public class Electrodomesticos extends Producto {
+    int garantia;
+    public Electrodomesticos(int id, String nombre, double precio, int garantia) {
+        super(id, nombre, precio*1.1);
+        this.garantia =garantia;
+    }
+    @Override
+    public String toString() {
+        return "Electrodomesticos {id=" + id + ", nombre='" + nombre + "', precio=" + precio + ", garantia" + garantia + "}";
+    }
+}
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,4 +1,6 @@
+import edu.uca.Electrodomesticos;
 import edu.uca.Producto;
+import edu.uca.Ropa;
 import edu.uca.Tienda;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -7,23 +9,33 @@ public class Tests {
 
     @Test
     public void productToStringTest(){
-        Producto producto = new Producto(1, "producto de prueba", 200);
+        Producto producto = new Ropa(11, "Ropa de prueba", 200);
         System.out.println(producto);
+        Producto electro = new Electrodomesticos(21, "electro de prueba", 500, 6);
+        System.out.println(electro);
     }
 
     @Test
     public void tiendaAgregarProductoTest(){
         Tienda tienda = new Tienda();
-        Producto producto = new Producto(1, "producto de prueba", 200);
+        Producto producto = new Ropa(11, "ropa de prueba", 200);
         tienda.agregarProducto(producto);
         Assert.assertEquals(tienda.getSize(), 1);
+
+        Producto electro = new Electrodomesticos(21, "electro de prueba", 500, 6);
+        tienda.agregarProducto(producto);
+        Assert.assertEquals(tienda.getSize(), 1);
+
     }
 
     @Test
     public void mostrarInventarioTest(){
         Tienda tienda = new Tienda();
-        Producto producto = new Producto(1, "producto de prueba", 200);
+        Producto producto = new Ropa(11, "ropa de prueba", 200);
         tienda.agregarProducto(producto);
+        tienda.mostrarInventario();
+        Producto electro = new Electrodomesticos(21, "electro de prueba", 500, 6 );
+        tienda.agregarProducto(electro);
         tienda.mostrarInventario();
     }
 }
